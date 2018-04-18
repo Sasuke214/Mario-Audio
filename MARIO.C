a1e1cf907a55014c01f7ccff6623bd349cc77e2b
Sasuke214 <dahalmanjil2016@gmail.com>
2018-04-19 04:03:09 +0530

    Add files via upload

---
+++ MARIO.C
@@ -0,0 +1,359 @@
+//it is the audio frequency of mario game
+#include<windows.h>
+#include<dos.h>
+void main()
+{
+sound(660 );
+delay(150);
+sound(660 );
+delay(300);
+sound(660 );
+delay(300);
+sound(510 );
+delay(100);
+sound(660 );
+delay(300);
+sound(770 );
+delay(550);
+sound(380 );
+delay(575);
+
+sound(510 );
+delay(450);
+sound(380 );
+delay(400);
+sound(320 );
+delay(500);
+sound(440 );
+delay(300);
+sound(480);
+delay(330);
+sound(450 );
+delay(150);
+sound(430 );
+delay(300);
+sound(380 );
+delay(200);
+sound(660);
+delay(200);
+sound(760);
+delay(150);
+sound(860 );
+delay(300);
+sound(700);
+delay(150);
+sound(760);
+delay(350);
+sound(660);
+delay(300);
+sound(520);
+delay(150);
+sound(580);
+delay(150);
+sound(480);
+delay(500);
+
+sound(510 );
+delay(450);
+sound(380 );
+delay(400);
+sound(320 );
+delay(500);
+sound(440);
+delay(300);
+sound(480);
+delay(330);
+sound(450 );
+delay(150);
+sound(430 );
+delay(300);
+sound(380 );
+delay(200);
+sound(660);
+delay(200);
+sound(760);
+delay(150);
+sound(860 );
+delay(300);
+sound(700);
+delay(150);
+sound(760);
+delay(350);
+sound(660);
+delay(300);
+sound(520);
+delay(150);
+sound(580);
+delay(150);
+sound(480);
+delay(500);
+
+sound(500 );
+delay(300);
+
+sound(760 );
+delay(100);
+sound(720 );
+delay(150);
+sound(680 );
+delay(150);
+sound(620);
+delay(300);
+
+sound(650);
+delay(300);
+sound(380 );
+delay(150);
+sound(430 );
+delay(150);
+
+sound(500 );
+delay(300);
+sound(430 );
+delay(150);
+sound(500 );
+delay(100);
+sound(570 );
+delay(220);
+
+sound(500 );
+delay(300);
+
+sound(760 );
+delay(100);
+sound(720 );
+delay(150);
+sound(680 );
+delay(150);
+sound(620);
+delay(300);
+
+sound(650);
+delay(300);
+
+sound(1020);
+delay(300);
+sound(1020);
+delay(150);
+sound(1020);
+delay(300);
+
+sound(380 );
+delay(300);
+sound(500 );
+delay(300);
+
+sound(760 );
+delay(100);
+sound(720 );
+delay(150);
+sound(680);
+delay(150);
+sound(620);
+delay(300);
+
+sound(650);
+delay(300);
+sound(380 );
+delay(150);
+sound(430 );
+delay(150);
+
+sound(500 );
+delay(300);
+sound(430 );
+delay(150);
+sound(500 );
+delay(100);
+sound(570 );
+delay(420);
+
+sound(585 );
+delay(450);
+
+sound(550 );
+delay(420);
+
+sound(500 );
+delay(360);
+
+sound(380 );
+delay(300);
+sound(500 );
+delay(300);
+sound(500 );
+delay(150);
+sound(500 );
+delay(300);
+
+sound(500 );
+delay(300);
+
+sound(760 );
+delay(100);
+sound(720 );
+delay(150);
+sound(680 );
+delay(150);
+sound(620);
+delay(300);
+
+sound(650);
+delay(300);
+sound(380 );
+delay(150);
+sound(430 );
+delay(150);
+
+sound(500 );
+delay(300);
+sound(430 );
+delay(150);
+sound(500 );
+delay(100);
+sound(570 );
+delay(220);
+
+sound(500 );
+delay(300);
+
+sound(760 );
+delay(100);
+sound(720 );
+delay(150);
+sound(680 );
+delay(150);
+sound(620);
+delay(300);
+
+sound(650);
+delay(300);
+
+sound(1020);
+delay(300);
+sound(1020);
+delay(150);
+sound(1020);
+delay(300);
+
+sound(380 );
+delay(300);
+sound(500 );
+delay(300);
+
+sound(760 );
+delay(100);
+sound(720 );
+delay(150);
+sound(680 );
+delay(150);
+sound(620);
+delay(300);
+
+sound(650);
+delay(300);
+sound(380 );
+delay(150);
+sound(430 );
+delay(150);
+
+sound(500 );
+delay(300);
+sound(430 );
+delay(150);
+sound(500 );
+delay(100);
+sound(570 );
+delay(420);
+
+sound(585 );
+delay(450);
+
+sound(550 );
+delay(420);
+
+sound(500 );
+delay(360);
+
+sound(380 );
+delay(300);
+sound(500 );
+delay(300);
+sound(500 );
+delay(150);
+sound(500 );
+delay(300);
+
+sound(500);
+delay(150);
+sound(500);
+delay(300);
+sound(500);
+delay(350);
+sound(500);
+delay(150);
+sound(580);
+delay(350);
+sound(660);
+delay(150);
+sound(500);
+delay(300);
+sound(430);
+delay(150);
+sound(380);
+delay(600);
+
+sound(500);
+delay(150);
+sound(500);
+delay(300);
+sound(500);
+delay(350);
+sound(500);
+delay(150);
+sound(580);
+delay(150);
+sound(660);
+delay(550);
+
+sound(870);
+delay(325);
+sound(760);
+delay(600);
+
+sound(500);
+delay(150);
+sound(500);
+delay(300);
+sound(500);
+delay(350);
+sound(500);
+delay(150);
+sound(580);
+delay(350);
+sound(660);
+delay(150);
+sound(500);
+delay(300);
+sound(430);
+delay(150);
+sound(380);
+delay(600);
+
+sound(660 );
+delay(150);
+sound(660 );
+delay(300);
+sound(660 );
+delay(300);
+sound(510 );
+delay(100);
+sound(660 );
+delay(300);
+sound(770 );
+delay(550);
+sound(380);
+delay(575);
+nosound();
+getch();
+}
